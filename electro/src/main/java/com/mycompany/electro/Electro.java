48eaa43452e494d82ce56ae0634ee2b7bdfea730
ronardj01 <ronardj01@gmail.com>
2024-07-29 10:16:37 +0200

    first commit

---
+++ electro/src/main/java/com/mycompany/electro/Electro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electro;
+
+/**
+ *
+ * @author ronar
+ */
+public class Electro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
